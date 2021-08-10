71db11849d5fb9a4a5e1e0a75e1b1d1471f3af47
aberu <aberuking19@yahoo.co.jp>
2021-08-10 15:11:08 +0000

    for heroku2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,18 +25,7 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
-  def followings
-    @user = User.find(params[:id])
-    @followings = @user.followings.page(params[:page])
-    counts(@user)
-  end
-  
-  def followers
-    @user = User.find(params[:id])
-    @followers = @user.followers.page(params[:page])
-    counts(@user)
-  end
+
   
   def likes
     @user = User.find(params[:id])
